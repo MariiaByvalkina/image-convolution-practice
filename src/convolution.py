d58d41558eb7a2f15e20971b5f444f18b3bd2cdd
Maria Byvalkina <masabyvalkina67715@gmail.com>
2026-05-18 23:31:34 +0300

    fix linters mistakes too

--- src/convolution.py
+++ src/convolution.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from typing import Optional
 
+
 class ImageConvolution:
 
     def __init__(self, kernel: np.ndarray, mode: str = "zero"):
@@ -10,12 +11,12 @@ class ImageConvolution:
         self.mode: str = mode
         self.result: Optional[np.ndarray] = None
 
-    def set_mode(self, mode: str) -> 'ImageConvolution':
+    def set_mode(self, mode: str) -> "ImageConvolution":
         self.mode = mode
         return self
 
-    def image_to_BW(self, image: str) -> 'ImageConvolution':
-        img = Image.open(image).convert('L')
+    def image_to_BW(self, image: str) -> "ImageConvolution":
+        img = Image.open(image).convert("L")
         self.image = np.array(img) / 255
         return self
 
@@ -25,42 +26,58 @@ class ImageConvolution:
 
         height, width = self.image.shape
 
-        padded = np.zeros((height + 2 * padding_height, width + 2 * padding_width))
+        padded = np.zeros(
+            (height + 2 * padding_height, width + 2 * padding_width)
+        )
 
-        padded[padding_height:padding_height + height, padding_width:padding_width + width] = self.image
+        padded[
+            padding_height: padding_height + height,
+            padding_width: padding_width + width,
+        ] = self.image
 
-        if self.mode == 'zero':
+        if self.mode == "zero":
             pass
 
-        if self.mode == 'edge':
-            padded[:padding_height, padding_width: padding_width + width] = self.image[0, :]
-            padded[:padding_height + height, padding_width: padding_width + width] = self.image[-1, :]
+        if self.mode == "edge":
+            padded[:padding_height, padding_width: padding_width + width] = (
+                self.image[0, :]
+            )
+            padded[: padding_height + height, padding_width: padding_width + width] = (
+                self.image[-1, :]
+            )
 
             for i in range(padding_width):
                 padded[:, i] = padded[:, padding_width]
-                padded[:, -i-1] = padded[:, -padding_width - 1]
+                padded[:, -i - 1] = padded[:, -padding_width - 1]
 
-        if self.mode == 'reflect':
-            padded[:padding_height, padding_width: padding_width + width] = self.image[1: padding_height + 1, :][::-1]
-            padded[padding_height + height:, padding_width: padding_width + width] = self.image[ -padding_height - 1: -1, :][::-1]
+        if self.mode == "reflect":
+            padded[:padding_height, padding_width: padding_width + width] = (
+                self.image[1: padding_height + 1, :][::-1]
+            )
+            padded[
+                padding_height + height:, padding_width: padding_width + width
+            ] = self.image[-padding_height - 1: -1, :][::-1]
 
             for i in range(padding_width):
                 padded[:, padding_width - 1 - i] = padded[:, padding_width + i + 1]
-                padded[:, padding_width - width + i] = padded[:, padding_width + width - 2 - i]
+                padded[:, padding_width - width + i] = (
+                    padded[:, padding_width + width - 2 - i]
+                )
 
         return padded
 
-    def convolve(self, normal: bool = True) -> 'ImageConvolution':
+    def convolve(self, normal: bool = True) -> "ImageConvolution":
         padded_image = self.padding()
         kernel_height, kernel_width = self.kernel.shape
-        padding_height, padding_width = kernel_height // 2, kernel_width // 2
         height, width = self.image.shape
 
         output = np.zeros((height, width))
 
         for i in range(height):
             for j in range(width):
-                region = padded_image[i: i + kernel_height, j: j + kernel_width]
+                region = padded_image[
+                    i: i + kernel_height, j: j + kernel_width
+                ]
                 output[i, j] = np.sum(region * self.kernel)
 
         if normal:
@@ -69,7 +86,7 @@ class ImageConvolution:
         self.result = output
         return self
 
-    def save_result(self, path: str) -> 'ImageConvolution':
+    def save_result(self, path: str) -> "ImageConvolution":
         result_image = Image.fromarray((self.result * 255).astype(np.uint8))
         result_image.save(path)
         return self
