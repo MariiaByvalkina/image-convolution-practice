55706a8bb15a129264e1dad6042aaff09747062b
Maria Byvalkina <masabyvalkina67715@gmail.com>
2026-06-01 20:05:40 +0300

    fix mistakes from tests

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 import numpy as np
 import os
@@ -6,40 +8,33 @@ from PIL import Image
 from convolution import ImageConvolution
 from kernels import get_kernel
 
-INPUT_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "../data/input.jpg")
-GOLDEN_DIR = os.path.join(os.path.dirname(__file__), "goldens")
+INPUT_IMAGE_PATH = Path(__file__).parent.parent / "data"
+GOLDEN_DIR = Path(__file__).parent / "goldens"
 
 os.makedirs(GOLDEN_DIR, exist_ok=True)
 
 kernels = ["sharpen", "blur", "sobel"]
 modes = ["edge", "reflect"]
 
-
-@pytest.fixture
-def sample_image():
-    if not os.path.exists(INPUT_IMAGE_PATH):
-        raise FileNotFoundError(
-            f"Положите картинку по пути '{INPUT_IMAGE_PATH}'"
-        )
-    img = Image.open(INPUT_IMAGE_PATH).convert('L')
-    return np.array(img, dtype=np.float64) / 255.0
+@pytest.fixture(params=list(INPUT_IMAGE_PATH.glob("*.jpg")))
+def sample_image(request):
+    return request.param.stem, Image.open(request.param)
 
 
 @pytest.mark.parametrize("kernel_name", kernels)
 @pytest.mark.parametrize("mode", modes)
-
 def test_convolution_integrity(sample_image, kernel_name, mode):
-    golden_path = os.path.join(GOLDEN_DIR, f"{kernel_name}_{mode}.png")
+    image_name, image = sample_image
+    golden_path = GOLDEN_DIR / f"{image_name}_{kernel_name}_{mode}.png"
 
     kernel = get_kernel(kernel_name)
-    tool = ImageConvolution(get_kernel(kernel_name), mode)
-    tool.convolve(sample_image)
-    result_uint8 = tool.get_result()
+    tool = ImageConvolution(kernel, mode)
+    result = tool.convolve(image)
 
-    if not os.path.exists(golden_path):
-        Image.fromarray(result_uint8, mode="L").save(golden_path)
-        pytest.skip(f"New golden created: {golden_path}")
+    if not golden_path.exists():
+        Image.fromarray(result.astype(np.uint8)).save(golden_path)
+        pytest.skip(f"Golden created: {golden_path}")
 
     expected = np.array(Image.open(golden_path))
-    assert np.array_equal(result_uint8, expected), f"Расхождение в режиме {kernel_name}_{mode}"
+    assert np.array_equal(result, expected), f"Расхождение в {image_name}_{kernel_name}_{mode}"
 
